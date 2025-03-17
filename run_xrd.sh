b52c436c010f3783edb29c6163394d7103970507
Danila Shiryaev <dshirya@gmail.com>
2025-03-17 10:44:05 -0400

    update

--- run_xrd.sh
+++ run_xrd.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Change to the directory where main.py is located.
-cd dirname 
+cd /Users/danila/Documents/GitHub/xrd-tool
+conda activate XRD-tool
 
 # Start the Dash server in the background.
-python3 main.py 
+python3 main.py & 
 
 # Wait a few seconds to ensure the server has started.
-sleep 3
+sleep 2
 
 # Open the default browser at the Dash app URL.
-open http://127.0.0.1:8050
+open http://127.0.0.1:8050/
 
 # Wait for the background process to finish.
 wait
